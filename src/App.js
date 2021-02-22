6058e590144f0813f606d903b198594db43244a4
Sakib Noman <skbnmn@gmail.com>
2021-02-23 00:56:19 +0600

    jsx props learnt

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name="Sakib" job="Engineer" hobby="Gardening" ></Person>
+        <Person name="Jahangir" job="Doctor" hobby="Reading Books"></Person>
+        <Person name="Alomgir" job="Private Employee" hobby="Reading Novels"></Person>
+        <Person name="Nowfel" job="Private Employee" hobby="Reading Novels"></Person>
+        <Person name="Mark Joke" job="Private Employee" hobby="Reading Novels"></Person>
+        <Person name="Kobir" job="Private Employee" hobby="Reading Novels"></Person>
+        <Person name="Sazid" job="Private Employee" hobby="Reading Novels"></Person>
+        <Person name="Sadik" job="Private Employee" hobby="Reading Novels"></Person>
       </header>
+    </div>
+  );
+}
+function Person(props) {
+  var style = {
+    border: '5px solid yellow',
+    margin: '10px',
+    padding: '15px',
+    width: '600px'
+  }
+  return (
+    <div style={style}>
+      <h1>{"Name: " + props.name}</h1>
+      <h3>{"Profession: " + props.job}</h3>
+      <h3>{"Hobby: " + props.hobby}</h3>
     </div>
   );
 }
