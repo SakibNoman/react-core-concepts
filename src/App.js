3ca77ea217c6198edbc101e13337d8686b70dad5
Sakib Noman <skbnmn@gmail.com>
2021-02-23 16:11:59 +0600

    learnt state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const product = [{
+    name: 'Photoshop', price: '$ 90.66'
+  }, {
+    name: 'Illustrator', price: '$ 66.55'
+  }, {
+    name: 'Lightroom', price: '$ 22.20'
+  },
+  {
+    name: 'Adobe Reader', price: '$ 2.20'
+  }]
   return (
     <div className="App">
       <header className="App-header">
-        <Person name="Sakib" job="Engineer" hobby="Gardening" ></Person>
-        <Person name="Jahangir" job="Doctor" hobby="Reading Books"></Person>
-        <Person name="Alomgir" job="Private Employee" hobby="Reading Novels"></Person>
-        <Person name="Nowfel" job="Private Employee" hobby="Reading Novels"></Person>
-        <Person name="Mark Joke" job="Private Employee" hobby="Reading Novels"></Person>
-        <Person name="Kobir" job="Private Employee" hobby="Reading Novels"></Person>
-        <Person name="Sazid" job="Private Employee" hobby="Reading Novels"></Person>
-        <Person name="Sadik" job="Private Employee" hobby="Reading Novels"></Person>
+        <Users></Users>
+        <Counter></Counter>
+        <Counter></Counter>
+        {
+          product.map(pd => <Product product={pd} ></Product>)
+        }
       </header>
     </div>
   );
 }
+
+function Users() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>Dynamic Users:</h2>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)} >Decrease</button>
+      <button onClick={() => setCount(count + 1)} >Increase</button>
+    </div>
+  )
+}
+
+function Product(props) {
+  const style = {
+    backgroundColor: 'lightgrey',
+    width: '200px',
+    height: '200px',
+    float: 'left',
+    color: 'black',
+    borderRadius: '5px'
+  }
+  return (
+    <div style={style}>
+      <h3>{props.product.name}</h3>
+      <h4>{props.product.price}</h4>
+      <button>Buy Now</button>
+    </div>
+
+  )
+}
+
 function Person(props) {
-  var style = {
+  const style = {
     border: '5px solid yellow',
     margin: '10px',
     padding: '15px',
